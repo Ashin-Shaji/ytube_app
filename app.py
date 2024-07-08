b540257d39e0ac773787e4815b7be32c4d7ccd67
Ashin-Shaji <122869032+Ashin-Shaji@users.noreply.github.com>
2024-07-08 14:48:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -366,7 +366,11 @@ def main4():
     video_url = st.text_input("Enter YouTube video URL:", disabled=st.session_state['video_selected'] or st.session_state['file_uploaded'] or st.session_state['existing_video_selected'])
 
     uploaded_file = st.file_uploader("Or upload an MP4 file", type=["mp4"], disabled=st.session_state['video_selected'] or bool(video_url) or st.session_state['existing_video_selected'])
-    outputt_path = '/content/Untitled Folder'
+    outputt_path = '/content/Untitled Folder' 
+  
+    if not os.path.exists(outputt_path):
+    os.makedirs(outputt_path)
+  
     existing_videos = [f for f in os.listdir(outputt_path) if f.endswith('.mp4')]
     selected_video = st.selectbox("Or select an existing video", [""] + existing_videos, index=0, disabled=bool(video_url) or uploaded_file is not None)
 
