79959e176b0b9f076474b180b27e2cd21de2986f
Ashin-Shaji <122869032+Ashin-Shaji@users.noreply.github.com>
2024-07-08 10:36:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,437 @@
+import streamlit as st
+import re, os, ast, speech_recognition as sr
+import google.generativeai as genai
+from datetime import datetime, timedelta
+from youtube_transcript_api import YouTubeTranscriptApi
+from pytube import YouTube
+from pydub import AudioSegment
+import time
+
+prompt="""You are Youtube video summarizer. You will be taking the transcript text and summarizing the entire video and providing
+                    the overall summary get the major points discussed,never mention the name odf the person
+                    within 250 words in following format. Please provide the summary of the text given here:"""
+
+def get_video_duration(youtube_video_url):
+    try:
+        # Fetch video details using pytube
+        yt = YouTube(youtube_video_url)
+        duration = yt.length  # Duration in seconds
+
+        # Convert duration to minutes and seconds
+        minutes, seconds = divmod(duration, 60)
+        duration_formatted = f"{minutes} minutes and {seconds} seconds"
+
+        return duration_formatted
+
+    except Exception as e:
+        raise e
+
+# Function to extract transcript details
+def extract_transcript_details(youtube_video_url):
+    try:
+        # Extract video ID from the URL
+        if "youtu.be" in youtube_video_url:
+            video_id = youtube_video_url.split("/")[-1]
+        else:
+            video_id = youtube_video_url.split("=")[1]
+
+        # Fetch the video transcript (you can specify the language)
+        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=("en",))  # English transcript
+
+        # Initialize a dictionary to store timestamps and transcriptions
+        transcript_dict = {}
+
+        # Extract text content and clean data
+        for t in transcript:
+            cleaned_text = re.sub(r"[^a-zA-Z0-9-ışğöüçiIŞĞÖÜÇİ ]", "", t["text"])
+            if cleaned_text:
+                timestamp = t["start"]
+                formatted_time = f"{int(timestamp // 3600):02d}:{int((timestamp // 60) % 60):02d}:{int(timestamp % 60):02d}"
+                transcript_dict[formatted_time] = cleaned_text
+
+        return transcript_dict
+
+    except Exception as e:
+        st.error(f"Error: {e}")
+        return None
+
+#for main2,3
+def extract_transcript_detailss(youtube_video_url):
+    try:
+        video_id=youtube_video_url.split("=")[1]
+
+        transcript_text=YouTubeTranscriptApi.get_transcript(video_id)
+
+        transcript = ""
+        for i in transcript_text:
+            transcript += " " + i["text"]
+
+        return transcript
+
+    except Exception as e:
+        raise e
+
+# Function to merge transcript
+def merge_transcript(transcript, interval_minutes=5):
+    try:
+        # Initialize variables
+        interval = timedelta(minutes=interval_minutes)
+        merged_transcript = {}
+        current_interval_start = datetime.strptime('00:00:00', '%H:%M:%S')
+        current_interval_end = current_interval_start + interval
+        current_text = ""
+
+        for timestamp, text in transcript.items():
+            time = datetime.strptime(timestamp, '%H:%M:%S')
+
+            # If the current timestamp is within the current interval, append the text
+            if time < current_interval_end:
+                current_text += text + " "
+            else:
+                # Save the current interval text to the merged_transcript
+                merged_transcript[current_interval_end.strftime('%H:%M:%S')] = current_text.strip()
+                # Move to the next interval
+                while time >= current_interval_end:
+                    current_interval_start = current_interval_end
+                    current_interval_end = current_interval_start + interval
+                # Start the new interval with the current text
+                current_text = text + " "
+
+        # Add the last interval if there's remaining text
+        if current_text:
+            merged_transcript[current_interval_end.strftime('%H:%M:%S')] = current_text.strip()
+
+        # Ensure the last key matches the last key from the input transcript
+        last_key_input = list(transcript.keys())[-1]
+        last_key_output = list(merged_transcript.keys())[-1]
+        if last_key_input != last_key_output:
+            merged_transcript[last_key_input] = merged_transcript.pop(last_key_output)
+
+        return merged_transcript
+
+    except Exception as e:
+        st.error(f"Error: {e}")
+        return None
+
+def generate_gemini_content(transcript_text, prompt, max_output_tokens=None):
+    try:
+        os.environ['GOOGLE_API_KEY'] = 'AIzaSyBbepUh8x3CqpkxNFnJ1IX0dFc0UNTwwbU'  # Replace with your API key
+        genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
+
+        models = [m for m in genai.list_models() if "text-bison" in m.name]
+        model = models[0].name
+
+        if max_output_tokens is None:
+            max_output_tokens = 300
+
+        # Generate text using the specified model
+        completion = genai.generate_text(
+            model=model,
+            prompt=transcript_text + prompt,
+            temperature=0.1,
+            max_output_tokens=max_output_tokens
+        )
+
+        output = completion.result.split('\n')
+        response = "\n".join(output)
+        return response
+    except Exception as e:
+        st.error("Can't generate summary")
+        return ""
+
+def convert_to_wav(input_file, output_file):
+    try:
+        audio = AudioSegment.from_file(input_file)
+        audio.export(output_file, format="wav")
+        print(f"Converted {input_file} to {output_file}")
+    except Exception as e:
+        print(f"An error occurred while converting to WAV: {e}")
+
+def delete_file(file_path):
+    try:
+        os.remove(file_path)
+        print(f"Deleted file: {file_path}")
+    except Exception as e:
+        print(f"An error occurred while deleting the file: {e}")
+
+def download_audio(video_url, output_path):
+    try:
+        yt = YouTube(video_url)
+        video_title = yt.title
+        audio_stream = yt.streams.filter(only_audio=True).first()
+        downloaded_file = audio_stream.download(output_path=output_path)
+        # st.success(f"Audio downloaded successfully to {downloaded_file}")
+        return downloaded_file, video_title
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+        return None, None
+
+def transcribe_audio(file_path, language='en-US', retries=3):
+    r = sr.Recognizer()
+    for attempt in range(retries):
+        try:
+            with sr.AudioFile(file_path) as source:
+                audio_text = r.record(source)
+                text = r.recognize_google(audio_text, language=language)
+                print("Transcription completed successfully")
+                return text
+        except sr.UnknownValueError:
+            print("Could not understand audio")
+            return ""
+        except sr.RequestError as req:
+            print(f"Error fetching results from Google Web Speech API: {req}")
+            if attempt < retries - 1:
+                time.sleep(2)  # Wait for 2 seconds before retrying
+            else:
+                return f"Transcription failed after {retries} attempts."
+
+#streamlit main
+#summarizer
+def main1():
+    st.markdown(
+            """
+            <style>
+            .stButton > button {
+                display: block;
+                margin: 0 auto;
+            }
+            </style>
+            """, unsafe_allow_html=True)
+    # Streamlit UI
+    st.title("YouTube Video Transcript Summarizer")
+    # Initialize session state variables
+    if 'youtube_link' not in st.session_state:
+        st.session_state.youtube_link = ""
+    if 'interval_minutes' not in st.session_state:
+        st.session_state.interval_minutes = 5
+    if 'transcript_extracted' not in st.session_state:
+        st.session_state.transcript_extracted = False
+    if 'merged_text' not in st.session_state:
+        st.session_state.merged_text = ""
+    if 'show_transcript' not in st.session_state:
+        st.session_state.show_transcript = False
+
+    youtube_link = st.text_input("Enter YouTube Video Link:")
+
+    if youtube_link:
+        st.session_state.youtube_link = youtube_link
+
+    col1, col2 = st.columns([5, 3])
+
+    if st.session_state.youtube_link:
+        with col1:
+            video_id = st.session_state.youtube_link.split("=")[1]
+            st.video(f"https://www.youtube.com/watch?v={video_id}", format="video/mp4")
+        dur = get_video_duration(st.session_state.youtube_link)
+        st.info(dur)
+        # interval_minutes = st.slider("Select interval in minutes", min_value=1, max_value=10, value=5, step=1)
+        # if interval_minutes:
+        #     st.session_state.interval_minutes = interval_minutes
+
+        if st.button('Get Summary'):
+            with st.spinner("Extracting transcript..."):
+                try:
+                    transcript_text = extract_transcript_details(st.session_state.youtube_link)
+                    if transcript_text:
+                        st.success("Transcript extracted successfully!")
+                        try:
+                            # merged_transcript = merge_transcript(transcript_text, st.session_state.interval_minutes)
+                            merged_transcript = merge_transcript(transcript_text, 4)
+                            if merged_transcript:
+                                merged_text = ""
+                                for timestamp, text in merged_transcript.items():
+                                    merged_text += f"{timestamp}: {text}\n"
+                                st.session_state.merged_text = merged_text
+                                st.session_state.transcript_extracted = True
+                                if st.session_state.transcript_extracted:
+                                    with st.expander('Show Transcript'):
+                                        st.session_state.show_transcript = True
+                                        if st.session_state.show_transcript:
+                                            st.write(st.session_state.merged_text)
+                                try:
+                                    res = generate_gemini_content(str(merged_text),prompt)
+                                    st.markdown('## **Summary**')
+                                    st.write(res)
+                                except:
+                                    st.exception('Failed to get summary')
+                        except Exception as e:
+                            st.exception(f"Failed to merge transcript: {e}")
+                except Exception as e:
+                    st.exception(f"Failed to extract transcript: {e}")
+
+#keyword extractor
+def main2():
+    st.title("YouTube Video Keyword Content Analyzer")
+
+    # Input for YouTube video link
+    youtube_link = st.text_input("Enter YouTube Video Link:")
+    col1, col2 = st.columns([5, 3])
+    if youtube_link:
+        with col1:
+          video_id = st.session_state.youtube_link.split("=")[1]
+          st.video(f"https://www.youtube.com/watch?v={video_id}", format="video/mp4")
+            # st.video(f"https://www.youtube.com/watch?v={youtube_link.split("=")[1]}", format="video/mp4")
+        # Prompt for extracting keywords
+        prompt = """From the transcript of the video, identify the 10 core topics/keyterms discussed and get them into a proper Python list []
+        separated by commas. Note that the transcript may contain grammatical/wording errors. Never get meaningless words."""
+
+        # Extract transcript (you should replace this with your actual function)
+        transcript = extract_transcript_detailss(youtube_link)
+
+        # Generate content based on the transcript (replace with your function)
+        res = generate_gemini_content(transcript, prompt)
+
+        with st.expander('Show Transcript'):
+            st.markdown(transcript)
+
+        # Convert the result to a Python list
+        my_list = ast.literal_eval(res)
+
+        # Display multiselect box
+        options = st.multiselect("Select keywords", my_list)
+        st.write("You selected:", options)
+
+        if options:
+            # Prompt for concise explanation
+            prompt_explanation = f"""you are an assistant who can analyze the following youtube video transcript: {transcript}
+            and provide summary on what the transcript says about the following keywords: {options}, note that you should provide the
+            answers based on the transcript only"""
+            # Generate content for the explanation (replace with your function)
+            concise_explanation = generate_gemini_content(transcript, prompt_explanation)
+            st.subheader("Concise Explanation:")
+            st.markdown(concise_explanation)
+
+#quest answering
+def main3():
+    st.title("YouTube Video Question Answering")
+    # Input for YouTube video link
+    youtube_link = st.text_input("Enter YouTube Video Link:")
+    col1, col2 = st.columns([5, 3])
+    if youtube_link:
+        with col1:
+          video_id = st.session_state.youtube_link.split("=")[1]
+          st.video(f"https://www.youtube.com/watch?v={video_id}", format="video/mp4")
+            # st.video(f"https://www.youtube.com/watch?v={youtube_link.split("=")[1]}", format="video/mp4")
+        # User query input
+        user_query = st.text_area("Enter your query:")
+
+        # Prompt for extracting keywords
+        prompt = f"""you are an assistant who can analyze video transcript and answer user query: '{user_query}' in 50 words,
+        based on the transcript of the video, you will never generate random/creative/not genuine responses on your own,
+        never talk out of the context"""
+        # Extract transcript (you should replace this with your actual function)
+        transcript = extract_transcript_detailss(youtube_link)
+        with st.expander('Show Transcript'):
+            st.markdown(transcript)
+        # Generate content based on the transcript and user query (replace with your function)
+        if user_query:
+            res = generate_gemini_content(transcript, prompt, max_output_tokens=100)
+            st.subheader("Generated Response:")
+            st.markdown(f"**{res}**")
+
+def main4():
+    # Streamlit UI
+    st.title("YouTube/MP4 Audio Transcriber and Summarizer")
+    st.markdown("""<style>.stButton > button {display: block;margin: 0 auto;}</style>""", unsafe_allow_html=True)
+
+    output_path = './amios'
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    # Initialize session state variables
+    if 'video_selected' not in st.session_state:
+        st.session_state['video_selected'] = False
+    if 'file_uploaded' not in st.session_state:
+        st.session_state['file_uploaded'] = False
+    if 'existing_video_selected' not in st.session_state:
+        st.session_state['existing_video_selected'] = False
+
+    video_url = st.text_input("Enter YouTube video URL:", disabled=st.session_state['video_selected'] or st.session_state['file_uploaded'] or st.session_state['existing_video_selected'])
+
+    uploaded_file = st.file_uploader("Or upload an MP4 file", type=["mp4"], disabled=st.session_state['video_selected'] or bool(video_url) or st.session_state['existing_video_selected'])
+    outputt_path = '/content/Untitled Folder'
+    existing_videos = [f for f in os.listdir(outputt_path) if f.endswith('.mp4')]
+    selected_video = st.selectbox("Or select an existing video", [""] + existing_videos, index=0, disabled=bool(video_url) or uploaded_file is not None)
+
+    # Update session state based on user input
+    if selected_video and selected_video != "":
+        st.session_state['existing_video_selected'] = True
+        st.session_state['video_selected'] = True
+        st.session_state['file_uploaded'] = False
+        video_url = None
+        uploaded_file = None
+    elif uploaded_file:
+        st.session_state['file_uploaded'] = True
+        st.session_state['video_selected'] = False
+        st.session_state['existing_video_selected'] = False
+        video_url = None
+    elif video_url:
+        st.session_state['file_uploaded'] = False
+        st.session_state['video_selected'] = False
+        st.session_state['existing_video_selected'] = False
+
+    if selected_video and selected_video != "":
+        st.video(os.path.join(outputt_path, selected_video))
+    elif uploaded_file:
+        st.video(uploaded_file)
+    elif video_url:
+        video_id = video_url.split("v=")[-1] if "v=" in video_url else video_url.split("/")[-1].split("?")[0]
+        st.video(f"https://www.youtube.com/embed/{video_id}")
+
+    if st.button("Transcribe"):
+        with st.spinner('Processing...'):
+            if video_url:
+                downloaded_file, video_title = download_audio(video_url, output_path)
+            elif uploaded_file:
+                downloaded_file = os.path.join(output_path, uploaded_file.name)
+                with open(downloaded_file, "wb") as f:
+                    f.write(uploaded_file.getbuffer())
+                video_title = os.path.splitext(uploaded_file.name)[0]
+            elif selected_video:
+                downloaded_file = os.path.join(outputt_path, selected_video)
+                video_title = os.path.splitext(selected_video)[0]
+            else:
+                st.error("Please enter a YouTube URL, upload an MP4 file, or select an existing video.")
+                st.stop()
+
+            if downloaded_file:
+                wav_output_file = os.path.splitext(downloaded_file)[0] + '.wav'
+                convert_to_wav(downloaded_file, wav_output_file)
+                # delete_file(downloaded_file)
+
+                text = transcribe_audio(wav_output_file)
+                if text:
+                    transcription_filename = f"{video_title}.txt".replace(" ", "_").replace("/", "_")
+                    file_path = os.path.join(output_path, transcription_filename)
+                    with open(file_path, "w") as file:
+                        file.write(text)
+                    st.success("Transcription completed successfully")
+                    with st.expander("Transcript"):
+                        st.markdown(f"#### {video_title}\n\n{text}")
+
+                    # Generate summary using Gemini model
+                    prompt = """You are a YouTube transcript summarizer. You will take youtube video transcript and provide detailed
+                    summary about major points discussed within 250 words. Never mention the name of the person"""
+                    try:
+                        summary = generate_gemini_content(text, prompt)
+                        st.markdown('## **Summary**')
+                        st.markdown(summary)
+                    except Exception as e:
+                        st.error("Can't generate summary")
+
+                delete_file(wav_output_file)
+
+# if __name__ == "__main__":
+#     main4()
+selected_option = st.radio("Select an option:",
+      ["YouTube Video Transcript Summarizer", "YouTube Video Keyword Content Analyzer","YouTube Video Question Answering",
+       "YouTube/MP4 Audio Transcriber and Summarizer"])
+
+if selected_option == "YouTube Video Transcript Summarizer":
+    main1()
+elif selected_option == "YouTube Video Keyword Content Analyzer":
+    main2()
+elif selected_option == "YouTube Video Question Answering":
+    main3()
+elif selected_option == "YouTube/MP4 Audio Transcriber and Summarizer":
+    main4()
