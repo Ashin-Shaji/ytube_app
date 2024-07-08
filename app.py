9b86dc1bb831a832861705e02b61c25f2923ca3d
Ashin-Shaji <122869032+Ashin-Shaji@users.noreply.github.com>
2024-07-08 16:48:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -97,145 +97,9 @@ def merge_transcript(transcript, interval_minutes=5):
         st.error(f"Error: {e}")
         return None
 
-# def generate_gemini_content(transcript_text, prompt, max_output_tokens=None):
-#     try:
-#         st.status('Summarizing...')
-#         os.environ['GOOGLE_API_KEY'] = 'AIzaSyBbepUh8x3CqpkxNFnJ1IX0dFc0UNTwwbU'  # Replace with your API key
-#         genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
-
-#         models = [m for m in genai.list_models() if "text-bison" in m.name]
-#         model = models[0].name
-
-#         if max_output_tokens is None:
-#             max_output_tokens = 300
-
-#         # Generate text using the specified model
-#         completion = genai.generate_text(
-#             model=model,
-#             prompt=transcript_text + prompt,
-#             temperature=0.1,
-#             max_output_tokens=max_output_tokens
-#         )
-
-#         output = completion.result.split('\n')
-#         response = "\n".join(output)
-#         return response
-#     except Exception as e:
-#         st.error("Can't generate summary")
-#         return ""
-      
-# def delete_file(file_path):
-#     try:
-#         os.remove(file_path)
-#         print(f"Deleted file: {file_path}")
-#     except Exception as e:
-#         print(f"An error occurred while deleting the file: {e}")
-
-output_path = '/tmp/amios'
-if not os.path.exists(output_path):
-    os.makedirs(output_path)
-  
-# def convert_to_wav(input_file, output_file):
-#     try:
-#         st.status('Converting audio to WAV...')
-#         audio_clip = AudioFileClip(input_file)
-#         audio_clip.write_audiofile(output_file)
-#         print(f"Converted {input_file} to {output_file}")
-#     except Exception as e:
-#         print(f"An error occurred while converting to WAV: {e}")
-
-# def download_audio(video_url, output_path):
-#     try:
-#         st.status('Downloading audio...')
-#         yt = YouTube(video_url)
-#         video_title = yt.title
-#         audio_stream = yt.streams.filter(only_audio=True).first()
-#         downloaded_file = audio_stream.download(output_path=output_path)
-#         st.write(f"Audio downloaded successfully to {downloaded_file}")
-#         return downloaded_file, video_title
-#     except Exception as e:
-#         st.error(f"An error occurred: {e}")
-#         return None, None
-
-# def transcribe_audio(file_path, language='en-US', retries=3):
-#     r = sr.Recognizer()
-#     for attempt in range(retries):
-#         try:
-#             st.status('Transcribing...')
-#             with sr.AudioFile(file_path) as source:
-#                 audio_text = r.record(source)
-#                 text = r.recognize_google(audio_text, language=language)
-#                 st.write("Transcription completed successfully")
-#                 return text
-#         except sr.UnknownValueError:
-#             st.write("Could not understand audio")
-#             return ""
-#         except sr.RequestError as req:
-#             st.write(f"Error fetching results from Google Web Speech API: {req}")
-#             if attempt < retries - 1:
-#                 time.sleep(2)  # Wait for 2 seconds before retrying
-#             else:
-#                 return f"Transcription failed after {retries} attempts."
-
-#new
-# Function to download audio from YouTube using pytube and convert it to MP3
-def download_audio(video_url, output_path):
-    try:
-        st.status("Downloading audio...")
-        yt = YouTube(video_url)
-        video_title = yt.title
-        audio_stream = yt.streams.filter(only_audio=True).first()
-        audio_path = os.path.join(output_path, f"{video_title}.mp3")
-        audio_stream.download(output_path=output_path, filename="audio.mp4")
-
-        # Convert the downloaded video to MP3 using moviepy
-        video_path = os.path.join(output_path, "audio.mp4")
-        video_clip = VideoFileClip(video_path)
-        audio_clip = video_clip.audio
-        audio_clip.write_audiofile(audio_path)
-        st.success(f"Audio downloaded successfully to {audio_path}")
-        
-        return audio_path, video_title
-
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
-        return None, None
-
-# Function to convert MP3 to WAV using moviepy
-def convert_to_wav(input_file, output_file):
-    try:
-        st.status("Converting to WAV...")
-        audio_clip = AudioFileClip(input_file)
-        audio_clip.write_audiofile(output_file)
-        st.success(f"Converted {input_file} to {output_file}")
-    except Exception as e:
-        st.error(f"An error occurred while converting to WAV: {e}")
-
-# Function to transcribe audio using Google Web Speech API
-def transcribe_audio(file_path, language='en-US', retries=3):
-    r = sr.Recognizer()
-    for attempt in range(retries):
-        try:
-            st.status("Transcribing audio...")
-            with sr.AudioFile(file_path) as source:
-                audio_text = r.record(source)
-                text = r.recognize_google(audio_text, language=language)
-                st.success("Transcription completed successfully")
-                return text
-        except sr.UnknownValueError:
-            st.error("Could not understand audio")
-            return ""
-        except sr.RequestError as req:
-            st.error(f"Error fetching results from Google Web Speech API: {req}")
-            if attempt < retries - 1:
-                time.sleep(2)  # Wait for 2 seconds before retrying
-            else:
-                return f"Transcription failed after {retries} attempts."
-
-# Gemini content generation function
 def generate_gemini_content(transcript_text, prompt, max_output_tokens=None):
     try:
-        st.status("Generating summary...")
+        st.status('Summarizing...')
         os.environ['GOOGLE_API_KEY'] = 'AIzaSyBbepUh8x3CqpkxNFnJ1IX0dFc0UNTwwbU'  # Replace with your API key
         genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
 
@@ -259,6 +123,60 @@ def generate_gemini_content(transcript_text, prompt, max_output_tokens=None):
     except Exception as e:
         st.error("Can't generate summary")
         return ""
+      
+# def delete_file(file_path):
+#     try:
+#         os.remove(file_path)
+#         print(f"Deleted file: {file_path}")
+#     except Exception as e:
+#         print(f"An error occurred while deleting the file: {e}")
+
+output_path = '/tmp/amios'
+if not os.path.exists(output_path):
+    os.makedirs(output_path)
+  
+def convert_to_wav(input_file, output_file):
+    try:
+        st.status('Converting audio to WAV...')
+        audio_clip = AudioFileClip(input_file)
+        audio_clip.write_audiofile(output_file)
+        print(f"Converted {input_file} to {output_file}")
+    except Exception as e:
+        print(f"An error occurred while converting to WAV: {e}")
+
+def download_audio(video_url, output_path):
+    try:
+        st.status('Downloading audio...')
+        yt = YouTube(video_url)
+        video_title = yt.title
+        audio_stream = yt.streams.filter(only_audio=True).first()
+        downloaded_file = audio_stream.download(output_path=output_path)
+        st.write(f"Audio downloaded successfully to {downloaded_file}")
+        return downloaded_file, video_title
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+        return None, None
+
+def transcribe_audio(file_path, language='en-US', retries=3):
+    r = sr.Recognizer()
+    for attempt in range(retries):
+        try:
+            st.status('Transcribing...')
+            with sr.AudioFile(file_path) as source:
+                audio_text = r.record(source)
+                text = r.recognize_google(audio_text, language=language)
+                st.write("Transcription completed successfully")
+                return text
+        except sr.UnknownValueError:
+            st.write("Could not understand audio")
+            return ""
+        except sr.RequestError as req:
+            st.write(f"Error fetching results from Google Web Speech API: {req}")
+            if attempt < retries - 1:
+                time.sleep(2)  # Wait for 2 seconds before retrying
+            else:
+                return f"Transcription failed after {retries} attempts."
+              
 #summarizer
 def main1():
     st.markdown(
@@ -436,123 +354,6 @@ def main3():
             st.markdown(f"**{res}**")
 
 #mp4 
-# def main4():
-#     # Streamlit UI
-#     st.title("YouTube/MP4 Audio Transcriber and Summarizer")
-#     st.markdown("""<style>.stButton > button {display: block;margin: 0 auto;}</style>""", unsafe_allow_html=True)
-
-#     # Define output paths
-#     output_path = '/tmp/amios'
-    
-#     # Create folder if it doesn't exist
-#     if not os.path.exists(output_path):
-#         os.makedirs(output_path)
-
-#     # Initialize session state variables
-#     if 'video_selected' not in st.session_state:
-#         st.session_state['video_selected'] = False
-#     if 'file_uploaded' not in st.session_state:
-#         st.session_state['file_uploaded'] = False
-#     if 'existing_video_selected' not in st.session_state:
-#         st.session_state['existing_video_selected'] = False
-
-#     # User inputs
-#     video_url = st.text_input("Enter YouTube video URL:", disabled=st.session_state['video_selected'] or st.session_state['file_uploaded'] or st.session_state['existing_video_selected'])
-#     uploaded_file = st.file_uploader("Or upload an MP4 file", type=["mp4"], disabled=st.session_state['video_selected'] or bool(video_url) or st.session_state['existing_video_selected'])
-    
-#     # List existing videos
-#     existing_videos = [f for f in os.listdir(output_path) if f.endswith('.mp4')]
-#     selected_video = st.selectbox("Or select an existing video", [""] + existing_videos, index=0, disabled=bool(video_url) or uploaded_file is not None)
-
-#     # Update session state based on user input
-#     if selected_video and selected_video != "":
-#         st.session_state['existing_video_selected'] = True
-#         st.session_state['video_selected'] = True
-#         st.session_state['file_uploaded'] = False
-#         video_url = None
-#         uploaded_file = None
-#     elif uploaded_file:
-#         st.session_state['file_uploaded'] = True
-#         st.session_state['video_selected'] = False
-#         st.session_state['existing_video_selected'] = False
-#         video_url = None
-#     elif video_url:
-#         st.session_state['file_uploaded'] = False
-#         st.session_state['video_selected'] = False
-#         st.session_state['existing_video_selected'] = False
-
-#     # Display selected video
-#     if selected_video and selected_video != "":
-#         st.video(os.path.join(output_path, selected_video))
-#     elif uploaded_file:
-#         st.video(uploaded_file)
-#     elif video_url:
-#         video_id = video_url.split("v=")[-1] if "v=" in video_url else video_url.split("/")[-1].split("?")[0]
-#         st.video(f"https://www.youtube.com/embed/{video_id}")
-
-#     # Process video
-#     if st.button("Transcribe"):
-#         with st.spinner('Processing...'):
-#             try:
-#                 if video_url:
-#                     downloaded_file, video_title = download_audio(video_url, output_path)
-#                 elif uploaded_file:
-#                     downloaded_file = os.path.join(output_path, uploaded_file.name)
-#                     with open(downloaded_file, "wb") as f:
-#                         f.write(uploaded_file.getbuffer())
-#                     video_title = os.path.splitext(uploaded_file.name)[0]
-#                 elif selected_video:
-#                     downloaded_file = os.path.join(output_path, selected_video)
-#                     video_title = os.path.splitext(selected_video)[0]
-#                 else:
-#                     st.error("Please enter a YouTube URL, upload an MP4 file, or select an existing video.")
-#                     st.stop()
-
-#                 # Check if the downloaded file exists
-#                 if not os.path.exists(downloaded_file):
-#                     st.error("Downloaded file does not exist.")
-#                     st.stop()
-#                 else:
-#                     st.write(f"Downloaded file path: {downloaded_file}")
-
-#                 wav_output_file = os.path.splitext(downloaded_file)[0] + '.wav'
-#                 convert_to_wav(downloaded_file, wav_output_file)
-
-#                 # Debug: Check if the WAV file was created successfully
-#                 if os.path.exists(wav_output_file):
-#                     st.write(f"WAV file path: {wav_output_file}")
-#                 else:
-#                     st.error(f"Converted WAV file does not exist at {wav_output_file}")
-#                     st.stop()
-
-#                 text = transcribe_audio(wav_output_file)
-#                 if text:
-#                     transcription_filename = f"{video_title}.txt".replace(" ", "_").replace("/", "_")
-#                     file_path = os.path.join(output_path, transcription_filename)
-#                     with open(file_path, "w") as file:
-#                         file.write(text)
-#                     st.success("Transcription completed successfully")
-#                     st.write(f"Transcription file path: {file_path}")
-#                     with st.expander("Transcript"):
-#                         st.markdown(f"#### {video_title}\n\n{text}")
-
-#                     # Generate summary using Gemini model
-#                     prompt = """You are a YouTube transcript summarizer. You will take youtube video transcript and provide detailed
-#                     summary about major points discussed within 250 words. Never mention the name of the person"""
-#                     try:
-#                         summary = generate_gemini_content(text, prompt)
-#                         st.markdown('## **Summary**')
-#                         st.markdown(summary)
-#                     except Exception as e:
-#                         st.error("Can't generate summary")
-#             except PermissionError:
-#                 st.error("Permission error: Unable to create or write to the specified directory.")
-#             except FileNotFoundError as e:
-#                 st.error(f"File not found error: {e}")
-#             except Exception as e:
-#                 st.error(f"An error occurred: {e}")
-
-#new
 def main4():
     # Streamlit UI
     st.title("YouTube/MP4 Audio Transcriber and Summarizer")
