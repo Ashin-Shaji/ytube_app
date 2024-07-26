3d7d0817c6a9baa7fef30187efa00f21ecfb20b6
Ashin-Shaji <122869032+Ashin-Shaji@users.noreply.github.com>
2024-07-26 10:59:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -868,11 +868,11 @@ def main2():
 
         # Generate content based on the transcript (replace with your function)
         if not st.session_state.gemini_response:
-            st.session_state.gemini_response = generate_gemini_content(merged_text, prompt)
-            # o = gem.GenerativeModel('gemini-1.5-pro-latest')
-            # st.session_state.gemini_response = o.generate_content(f"""From the transcript of the video :{merged_text}, identify the 10 core topics/keyterms 
-            # discussed and get them into a proper clean Python list [] separated by commas. Note that the transcript may contain grammatical/wording errors.
-            # Never get meaningless words.""").text
+            # st.session_state.gemini_response = generate_gemini_content(merged_text, prompt)
+            o = gem.GenerativeModel('gemini-1.5-pro-latest')
+            st.session_state.gemini_response = o.generate_content(f"""From the transcript of the video :{merged_text}, identify the 10 core topics/keyterms 
+            discussed and get them into a proper clean Python list [] separated by commas. Note that the transcript may contain grammatical/wording errors.
+            Never get meaningless words.""").text
               
         with st.expander('Show Transcript'):
             st.markdown(merged_text)
